05ed3f4cc9291a509bea444d5409d81103a86216
Koras02 <wjdgh0727@gmail.com>
2025-02-21 04:57:47 +0900

    mysql 기본 개념

---
+++ 1.database/1-1.database-cr-use.sql
@@ -0,0 +1,5 @@
+-- 데이터 베이스 생성
+CREATE DATABASE KORAS02;
+
+-- 데이터 베이스 사용하기
+USE KORAS02;
---
+++ 1.database/1-4.table-alter.sql
@@ -0,0 +1,6 @@
+
+-- 테이블 열 추가 
+ALTER TABLE kimi ADD COLUMN age INT;
+
+-- 열 삭제
+ALTER TABLE kimi DROP COLUMN job;
